55c4a264677973aed501ed9e1ac7e7d2aab5e7e8
tomisin110 <ayoemma188@gmail.com>
2024-07-03 19:45:32 +0100

    SECOND commit

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-const dateTimeElement = document.querySelector('.date-time');
-const date = new Date();
+const dateTimeElement = document.querySelector('.day');
+        const date = new Date();
+        const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+        const d = weekday[date.getDay()];
+        dateTimeElement.textContent = `Current Day: ${d}`;
 
+        // Get the current time in UTC
+        const timeElement = document.querySelector('.time');
+        const utcHours = date.getUTCHours().toString().padStart(2, '0');
+        const utcMinutes = date.getUTCMinutes().toString().padStart(2, '0');
+        const utcSeconds = date.getUTCSeconds().toString().padStart(2, '0');
+        timeElement.textContent = `Current Time: ${utcHours}:${utcMinutes}:${utcSeconds} UTC`;
 
-dateTimeElement.textContent = `Current Date and Time: ${date}`;
+        // Update the time every second
+        setInterval(() => {
+            const date = new Date();
+            const utcHours = date.getUTCHours().toString().padStart(2, '0');
+            const utcMinutes = date.getUTCMinutes().toString().padStart(2, '0');
+            const utcSeconds = date.getUTCSeconds().toString().padStart(2, '0');
+            timeElement.textContent = `Current Time: ${utcHours}:${utcMinutes}:${utcSeconds} UTC`;
+        }, 1000);
